a1f1ff3188e7a4d6dfcbc33cefda5c5a7a8b3441
momchil-anachkov <momchil.anachkov@gmail.com>
2024-08-01 16:56:47 +0300

    virtual scroll

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,15 +1,44 @@
-import React, {UIEventHandler, UIEvent} from 'react';
+import React, {UIEventHandler, UIEvent, useEffect, useState, useRef} from 'react';
 import './Carousel.css';
 
 function Carousel(props: {
     width: string,
     height: string,
     images: { src: string }[]
+    virtualScroll: VirtualScrollConfig,
 }) {
-    const images = props.images.map((image, index) => {
-        // return <img key={index} width={image.width} height={image.height} src={image.src} alt="A random image"/>
+    const width = useRef(0);
+    const height = useRef(0);
+    const imageStart = useRef(0);
+    const shiftAheadPoint = useRef(props.virtualScroll.shiftAheadPoint);
+    const shiftBehindPoint = useRef(props.virtualScroll.shiftBehindPoint);
+
+    const imageTrackRef = useRef<HTMLDivElement | null>(null);
+
+    const [imagesToRender, setImagesToRender] = useState(props.images.slice(imageStart.current, props.virtualScroll.domImageCount));
+
+    useEffect(() => {
+        const updateSize = () => {
+            console.assert(imageTrackRef.current != null, 'trackRef was null');
+            const newWidth = imageTrackRef.current!.offsetWidth;
+            const newHeight = imageTrackRef.current!.offsetHeight;
+
+            width.current = newWidth;
+            height.current = newHeight;
+        }
+
+        updateSize();
+        window.addEventListener('resize', updateSize);
+        return () => {
+            console.log('removing resize')
+            window.removeEventListener('resize', updateSize);
+        }
+    }, []);
+
+    const images = imagesToRender.map((image, index) => {
         return <img width="100%" key={index} src={image.src} alt="A random image"/>
     });
+
 
     const imageTrackStyle = {
         width: props.width,
@@ -18,30 +47,47 @@ function Carousel(props: {
 
     const scrollHandler: UIEventHandler<HTMLDivElement> = (event: UIEvent<HTMLDivElement>) => {
         const target = event.target as HTMLDivElement;
+
+        const currentPosition = target.scrollLeft / width.current;
+
+        const movedPastTheShiftAheadPoint = imageStart.current + currentPosition > shiftAheadPoint.current;
+        const stillHaveNewImagesAhead = imageStart.current + props.virtualScroll.domImageCount < props.virtualScroll.totalImageCount;
+        if (movedPastTheShiftAheadPoint && stillHaveNewImagesAhead) {
+            imageStart.current += props.virtualScroll.shiftBy;
+            setImagesToRender(props.images.slice(imageStart.current, imageStart.current + props.virtualScroll.domImageCount));
+
+            shiftAheadPoint.current += props.virtualScroll.shiftBy;
+            shiftBehindPoint.current += props.virtualScroll.shiftBy;
+            target.scrollLeft -= props.virtualScroll.shiftBy * width.current;
+        }
+
+        const movedPastTheShiftBehindPoint = imageStart.current + currentPosition < shiftBehindPoint.current;
+        const stillHaveNewImagesBehind = imageStart.current > 0;
+        if (movedPastTheShiftBehindPoint && stillHaveNewImagesBehind) {
+            imageStart.current -= props.virtualScroll.shiftBy;
+            setImagesToRender(props.images.slice(imageStart.current, imageStart.current + props.virtualScroll.domImageCount));
+
+            shiftAheadPoint.current -= props.virtualScroll.shiftBy;
+            shiftBehindPoint.current -= props.virtualScroll.shiftBy;
+            target.scrollLeft += props.virtualScroll.shiftBy * width.current;
+        }
     }
 
     return (
         <div className="Carousel">
-            <div onScroll={scrollHandler} className="image-track" style={imageTrackStyle}>
+            <div onScroll={scrollHandler} className="image-track" ref={imageTrackRef} style={imageTrackStyle}>
                 {images}
             </div>
         </div>
     );
 }
 
-// TODO: Infinite scroll
-interface infiniteScrollConfig {
+export interface VirtualScrollConfig {
     totalImageCount: number;
     domImageCount: number;
-    loadAheadPoint: number;
-    loadBehindPoint: number;
-}
-
-interface infiniteScrollConfigExample {
-    totalImageCount: 200;
-    domImageCount: 50;
-    loadAheadPoint: 40;
-    loadBehindPoint: 10;
+    shiftAheadPoint: number;
+    shiftBehindPoint: number;
+    shiftBy: number;
 }
 
 export default Carousel;
--- src/Showcase.tsx
+++ src/Showcase.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Showcase.css';
-import Carousel from './Carousel';
+import Carousel, {VirtualScrollConfig} from './Carousel';
 
 function Showcase() {
 
@@ -11,12 +11,34 @@ function Showcase() {
         { src:`https://picsum.photos/${300}/${800}?random=${4}` },
         { src:`https://picsum.photos/${300}/${300}?random=${5}` },
         { src:`https://picsum.photos/${300}/${600}?random=${6}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${7}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${8}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${9}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${10}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${11}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${12}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${13}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${14}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${15}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${16}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${17}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${18}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${19}` },
+        { src:`https://picsum.photos/${300}/${600}?random=${20}` },
     ];
+
+    const virtualScrollConfig: VirtualScrollConfig = {
+        totalImageCount: 20,
+        domImageCount: 10,
+        shiftAheadPoint: 6,
+        shiftBehindPoint: 4,
+        shiftBy: 2,
+    }
 
     return (
         <div className="Showcase">
             <div className="section">
-                <Carousel width="100%" height="300px" images={images}/>
+                <Carousel width="100%" height="300px" images={images} virtualScroll={virtualScrollConfig}/>
             </div>
         </div>
     );
