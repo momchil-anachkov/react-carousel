7128738edc5cb3fa082bcae8a32a411e8b5ce53a
momchil-anachkov <momchil.anachkov@gmail.com>
2024-08-03 17:16:38 +0300

    fix scroll-snap & virtual scroll adjustment clash

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, {UIEventHandler, UIEvent, useEffect, useState, useRef} from 'react';
+import React, {UIEventHandler, UIEvent, useEffect, useState, useRef, useLayoutEffect} from 'react';
 import './Carousel.css';
 
 function Carousel(props: {
@@ -9,10 +9,12 @@ function Carousel(props: {
 }) {
     const width = useRef(0);
     const height = useRef(0);
+    // TODO: Preload images behind on startup
     const imageStart = useRef(0);
     const imageTrackRef = useRef<HTMLDivElement | null>(null);
     const [imagesToRender, setImagesToRender] = useState(props.images.slice(imageStart.current, props.virtualScroll.domImageCount));
     const scrollAdjustment = useRef(0);
+    const scrollEndTimeout = useRef(0);
 
     /* Keep track of the carousel size */
     useEffect(() => {
@@ -35,6 +37,17 @@ function Carousel(props: {
     const images = imagesToRender.map((image, index) => {
         return <img width="100%" key={index} src={image.src} alt="A random image"/>
     });
+
+    const realignOnScrollEnd = () => {
+        if (scrollEndTimeout.current !== 0) {
+            clearTimeout(scrollEndTimeout.current);
+        }
+
+        scrollEndTimeout.current = setTimeout(() => {
+            const currentPosition = Math.round(imageTrackRef.current!.scrollLeft / width.current);
+            imageTrackRef.current!.scrollTo({ left: currentPosition * width.current, behavior: 'smooth' });
+        }, 50) as any;
+    }
 
     const checkAndShiftVirtualWindow: UIEventHandler<HTMLDivElement> = (event: UIEvent<HTMLDivElement>) => {
         const target = event.target as HTMLDivElement;
@@ -83,16 +96,21 @@ function Carousel(props: {
         }
     }
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (scrollAdjustment.current !== 0) {
             imageTrackRef.current!.scrollLeft += scrollAdjustment.current;
             scrollAdjustment.current = 0;
         }
-    })
+    });
+
+    const onScroll: UIEventHandler<HTMLDivElement> = (event: UIEvent<HTMLDivElement>) => {
+        realignOnScrollEnd();
+        checkAndShiftVirtualWindow(event);
+    }
 
     return (
         <div className="Carousel">
-            <div onScroll={checkAndShiftVirtualWindow} className="image-track" ref={imageTrackRef}>
+            <div onScroll={onScroll} className="image-track" ref={imageTrackRef}>
                 {images}
             </div>
         </div>
